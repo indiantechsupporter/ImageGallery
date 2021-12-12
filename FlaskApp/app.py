bde7ff8ee545f7980d6f92c5c931b8ca7b60d5b6
Pascal Töpke <pascal.toepke@gmail.com>
2021-12-12 18:37:17 +0100

    created index and upload templates and included render templates

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'first app yeah'
+def index():
+    return render_template('index.html')
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 if __name__ == '__main__':
     app.run()
